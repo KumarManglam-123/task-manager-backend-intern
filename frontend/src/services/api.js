9f152ea1d46399094cb7323f751f336422810c19
KumarManglam-123 <kmanglam2003@gmail.com>
2026-02-18 01:01:47 +0530

    fix: production api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1"
+  baseURL: "https://task-manager-backend-intern.onrender.com/api/v1",
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
